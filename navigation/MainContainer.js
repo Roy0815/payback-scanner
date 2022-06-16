aed05e81a89d42a6bc45cd20ef6a8fab57099198
Roy0815 <roy.lotzwik@gmx.de>
2022-06-16 12:49:26 +0200

    Basic Camera & Image Picker

--- navigation/MainContainer.js
+++ navigation/MainContainer.js
@@ -1,6 +1,8 @@
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Ionicons from "react-native-vector-icons/Ionicons";
+import { Camera, CameraType } from "expo-camera";
 
 // Screens
 import HomeScreen from "./screens/HomeScreen";
@@ -12,32 +14,38 @@ const settingsName = "Settings";
 
 const Tab = createBottomTabNavigator();
 
-export default function HomeContainer() {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName={homeName}
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-            let routeName = route.name;
+export default class HomeContainer extends React.Component {
+  async componentDidMount() {
+    await Camera.requestCameraPermissionsAsync();
+  }
 
-            switch (routeName) {
-              case homeName:
-                iconName = focused ? "home" : "home-outline";
-                break;
-              case settingsName:
-                iconName = focused ? "settings" : "settings-outline";
-                break;
-            }
+  render() {
+    return (
+      <NavigationContainer>
+        <Tab.Navigator
+          initialRouteName={homeName}
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+              let routeName = route.name;
 
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-        })}
-      >
-        <Tab.Screen name={homeName} component={HomeScreen} />
-        <Tab.Screen name={settingsName} component={SettingsScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
+              switch (routeName) {
+                case homeName:
+                  iconName = focused ? "home" : "home-outline";
+                  break;
+                case settingsName:
+                  iconName = focused ? "settings" : "settings-outline";
+                  break;
+              }
+
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+          })}
+        >
+          <Tab.Screen name={homeName} component={HomeScreen} />
+          <Tab.Screen name={settingsName} component={SettingsScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
--- navigation/screens/SettingsScreen.js
+++ navigation/screens/SettingsScreen.js
@@ -1,23 +1,54 @@
-import { View, StyleSheet, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Button, Image, View, StyleSheet, Text } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 
 export default function HomeScreen({ navigation }) {
+  const [image, setImage] = useState(null);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
+  const takePhoto = async () => {
+    let result = await ImagePicker.launchCameraAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
   return (
-    <View style={style.home}>
-      <Text>Settings Screen</Text>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Button title="Pick an image from camera roll" onPress={pickImage} />
+      <Button title="Take a picture" onPress={takePhoto} />
+      {image && (
+        <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
+      )}
     </View>
   );
 }
 
 const style = StyleSheet.create({
-  home: {
-    backgroundColor: "#fff",
+  container: {
     flex: 1,
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
   },
-  homeText: {
-    fontSize: 26,
-    fontWeight: "bold",
+  camera: {
+    flex: 1,
   },
 });
