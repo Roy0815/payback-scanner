927cd8898f88e0900e5be3f6c17165c91024a797
Roy0815 <roy.lotzwik@gmx.de>
2022-06-19 22:05:48 +0200

    added screen provider

---
+++ navigation/screens/index.js
@@ -0,0 +1,6 @@
+import SettingsScreen from "./SettingsScreen";
+import HomeScreen from "./HomeScreen";
+import AccountScreen from "./AccountScreen";
+import CaseDetailScreen from "./CaseDetailScreen";
+
+export { SettingsScreen, HomeScreen, AccountScreen, CaseDetailScreen };
