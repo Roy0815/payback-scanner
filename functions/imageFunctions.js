ddc1be3887a81d94c883c88705e4e5672a72211b
Roy0815 <roy.lotzwik@gmx.de>
2022-06-26 19:09:29 +0200

    added read & buffer capabilities

--- functions/imageFunctions.js
+++ functions/imageFunctions.js
@@ -2,15 +2,48 @@ import * as ImagePicker from "expo-image-picker";
 import * as MediaLibrary from "expo-media-library";
 
 const albumName = "Payback Scanner";
+var album = {};
+var imageBuffer = {};
 
 const _saveImage = async (uri) => {
+  console.log("before");
   let asset = await MediaLibrary.createAssetAsync(uri);
 
-  if (!(await MediaLibrary.getAlbumAsync(albumName))) {
-    await MediaLibrary.createAlbumAsync(albumName, asset, false);
+  if (!album) album = await MediaLibrary.getAlbumAsync(albumName);
+
+  if (!album) {
+    album = await MediaLibrary.createAlbumAsync(albumName, asset, false);
   } else {
-    await MediaLibrary.addAssetsToAlbumAsync(asset, albumName, false);
+    await MediaLibrary.addAssetsToAlbumAsync(asset, album, false);
   }
+};
+
+const _getImageFromDevice = async (name) => {
+  //get album
+  if (!album.id) album = await MediaLibrary.getAlbumAsync(albumName);
+
+  let result = await MediaLibrary.getAssetsAsync({
+    album: await MediaLibrary.getAlbumAsync(albumName),
+  });
+
+  //build regex and search
+  let regex = new RegExp(`.*\/${albumName}\/${name}\..*`);
+
+  let index = result.assets.findIndex((item) => regex.test(item.uri));
+
+  if (index != -1) {
+    //fill buffer and return
+    imageBuffer[name] = result.assets[index].uri;
+    return result.assets[index].uri;
+  }
+};
+
+const getImage = async (name) => {
+  //try from buffer
+  if (imageBuffer[name]) return imageBuffer[name];
+
+  //get from device
+  return _getImageFromDevice(name);
 };
 
 const pickImage = async () => {
@@ -24,8 +57,7 @@ const pickImage = async () => {
 
   if (!result.cancelled) {
     let saveResult = await _saveImage(result.uri);
-    console.log(result);
-    setImage(saveResult);
+    console.log(saveResult);
   }
 };
 
@@ -35,6 +67,13 @@ const takePhoto = async () => {
     allowsEditing: true,
     quality: 1,
   });
+
+  console.log(result);
+
+  if (!result.cancelled) {
+    let saveResult = await _saveImage(result.uri);
+    console.log(saveResult);
+  }
 };
 
-export { takePhoto, pickImage };
+export { takePhoto, pickImage, getImage };
