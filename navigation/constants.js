9f9d16ceb3ffeed8e3cb0aadf9c1ea0a6c3093a2
Roy0815 <roy.lotzwik@gmx.de>
2022-06-30 11:56:18 +0200

    moved screen names to constants file

---
+++ navigation/constants.js
@@ -0,0 +1,7 @@
+// Screen names
+const homeName = "Home";
+const caseDetailName = "Case Details";
+const accountName = "Account";
+const settingsName = "Settings";
+
+export { homeName, caseDetailName, accountName, settingsName };
